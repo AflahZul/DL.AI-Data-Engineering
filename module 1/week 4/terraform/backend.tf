907d908cfdd31c19ba9f42f350d150e40bbddd30
AZul <aflahzul96@gmail.com>
2026-02-05 15:08:56 +0800

    edit

--- module 1/week 4/terraform/backend.tf
+++ module 1/week 4/terraform/backend.tf
@@ -1,5 +1,5 @@
 terraform {
   backend "local" {
-    path = "/home/coder/.local/share/code-server/User/de-c1w4-480054813083-us-east-1-terraform-state.state"
+    path = "/home/coder/.local/share/code-server/User/de-c1w4-COURSERA-us-east-1-terraform-state.state"
   }
 }
